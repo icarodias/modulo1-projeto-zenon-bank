c4a668a882e51ebbc466b6e7710bc742ac57d018
icarodanieldiasdecarvalho <icarodiasdev@gmail.com>
2026-04-15 23:59:32 -0300

    handle errors of file fields

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionIngestor.java
@@ -4,47 +4,70 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class TransactionIngestor {
+    private final AtomicInteger atomicInter = new AtomicInteger(0);
 
-    public TransactionIngestor(){}
+    public TransactionIngestor(){
+    }
 
     public List<Transaction> obtainTransactionsFromCSV(String fileName, long amountLines) {
+
         Path path = Path.of(fileName);
         try (Stream<String> lines = Files.lines(path)) {
-            return lines
+            List<Transaction> transactions = lines
                     .skip(1)
                     .limit(amountLines)
-                    .map(this::parseToTransaction)
+                    .map(line -> this.parseToTransaction(line, atomicInter))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
                     .toList();
+
+            System.out.println("Metric: Total raws with error: " + atomicInter.get());
+            atomicInter.set(0);
+
+            return transactions;
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException("Error while read file");
         }
     }
 
-    private Transaction parseToTransaction(String transactionRaw) {
-        final String[] fields = transactionRaw.split(",");
+    private Optional<Transaction> parseToTransaction(String transactionRaw, AtomicInteger atomicInter) {
+        final String delimiterCSV = ",";
+        int fieldsInTransactionRaw = 11;
+        final String[] fields = transactionRaw.split(delimiterCSV);
 
-        if (fields.length != 11) {
-            throw new RuntimeException("Error while parse raw to transaction");
+        try {
+            if (fields.length != fieldsInTransactionRaw) {
+                throw new IllegalArgumentException("transaction raw not has correct number of fields: " + fields.length);
+            }
+
+            if (Arrays.stream(fields).anyMatch(field -> field.trim().isEmpty())) {
+                throw new IllegalArgumentException("transaction raw cannot have any empty fields");
+            }
+
+            TransactionCustomer transactionCustomerOrigin = new TransactionCustomer(
+                    fields[3], new BigDecimal(fields[4]), new BigDecimal(fields[5]));
+            TransactionCustomer transactionCustomerRecipient = new TransactionCustomer(
+                    fields[6], new BigDecimal(fields[7]), new BigDecimal(fields[8]));
+
+            return Optional.of(new Transaction(
+                    Integer.parseInt(fields[0]),
+                    TransactionType.valueOf(fields[1]),
+                    new BigDecimal(fields[2]),
+                    transactionCustomerOrigin,
+                    transactionCustomerRecipient,
+                    fields[9].equals("1"),
+                    fields[10].equals("1")
+            ));
+        } catch (Exception ex) {
+            System.err.println("Error: " + transactionRaw + " | " + ex.getMessage());
+            atomicInter.addAndGet(1);
+            return Optional.empty();
         }
-
-        TransactionCustomer transactionCustomerOrigin = new TransactionCustomer(
-                fields[3], new BigDecimal(fields[4]),new BigDecimal(fields[5]));
-        TransactionCustomer transactionCustomerRecipient =  new TransactionCustomer(
-                fields[6], new BigDecimal(fields[7]),new BigDecimal(fields[8]));
-
-        return new Transaction(
-                Integer.valueOf(fields[0]),
-                TransactionType.valueOf(fields[1]),
-                new BigDecimal(fields[2]),
-                transactionCustomerOrigin,
-                transactionCustomerRecipient,
-                fields[9].equals("1"),
-                fields[10].equals("1")
-        );
     }
 }
